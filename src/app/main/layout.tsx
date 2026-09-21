be5bdd903eac98c614efac1499d604d966aa5789
LEWeb and LEApp Studio <leccgamesandapps@gmail.com>
2026-09-21 18:52:13 +0800

    Use MainContent so padding is removed while in-game

--- src/app/main/layout.tsx
+++ src/app/main/layout.tsx
@@ -2,6 +2,7 @@ import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
 import { authOptions } from "@/lib/auth";
 import { MainNav } from "@/components/layout/MainNav";
+import { MainContent } from "@/components/layout/MainContent";
 
 export const dynamic = "force-dynamic";
 
@@ -12,7 +13,7 @@ export default async function MainLayout({ children }: { children: React.ReactNo
   return (
     <div className="flex min-h-screen bg-surface-950">
       <MainNav />
-      <main className="flex-1 lg:ml-0 pt-14 pb-20 lg:pt-0 lg:pb-0 overflow-x-hidden">{children}</main>
+      <MainContent>{children}</MainContent>
     </div>
   );
 }
